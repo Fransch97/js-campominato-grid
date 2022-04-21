e079c3cdfb22b02dba92434fa41e859a03c2add5
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-21 15:26:24 +0200

    first functions

--- components/js/script.js
+++ components/js/script.js
@@ -16,3 +16,47 @@
 // buon lavoro! :faccia_leggermente_sorridente:
 
 console.log("working");
+
+let container = document.querySelector('.container');
+
+//div generator
+function creatCubes(){
+    const cube = document.createElement('div');
+    
+    console.log(cube);
+}
+creatCubes()
+
+//controll evenen ore odd
+function isEvenOdd(number){
+    if(number % 2){
+        console.log("odd")
+        return 'odd'
+    }
+    console.log("even")
+    return 'even'
+}
+// isEvenOdd(5) tested 
+
+const randomNumber = []
+//random numbers from 1 to
+function oneToRandom(toNumber){
+    
+ for(let i = 0; i < toNumber; i++){
+    let exists, numberRandom;
+    while(!exists){
+        numberRandom = Math.ceil(Math.random()*toNumber)
+        console.log(numberRandom);
+        if(!randomNumber.includes(numberRandom)){
+            exists = true;
+            randomNumber.push(numberRandom);
+        }else{
+            console.log("esisteva già")
+        }
+    }
+     
+ }
+}
+
+oneToRandom(10)
+console.log(randomNumber)
