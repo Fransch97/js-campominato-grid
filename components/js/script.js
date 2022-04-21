17a837e270983ff92c19d88f27a9e340242e5155
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-21 23:35:03 +0200

    all bombs show

--- components/js/script.js
+++ components/js/script.js
@@ -1,25 +1,3 @@
-// Esercizio di oggi: **Griglia Campo Minato**
-// nome repo: js-campominato-grid
-// **Consegna**
-// L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
-// con difficoltà 1 => tra 1 e 100
-// con difficoltà 2 => tra 1 e 81
-// con difficoltà 3 => tra 1 e 49
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
-// **Consigli del giorno:** :party_wizard:
-// **** Creiamo prima una griglia unica (es con 100 quadratini) per  poi dinamicizzare il dato con classi css dedicate
-// **** Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio:
-// Di cosa ho bisogno per generare i numeri?
-// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-// Le validazioni e i controlli possiamo farli anche in un secondo momento.
-// buon lavoro! :faccia_leggermente_sorridente:
-
-
-
-
-
-
 console.log("working");
 
 const container = document.querySelector('.container');
@@ -43,7 +21,6 @@ play.addEventListener('click', ()=>{
    
 }
 )
-
 //div generator
 function creatCubes(number,randomico,level){
     const cube = document.createElement('div');
@@ -71,16 +48,22 @@ function creatCubes(number,randomico,level){
                 <h1>You lose <br> Replay!</h1>
             </div>
             `;
+            let oddSS = document.querySelectorAll('.odd');
+            console.log(oddSS);
+            for(let i = 1; i < oddSS.length; i++){
+                let addossunique = oddSS[i];
+                console.log(addossunique)
+                addossunique.classList.add('clicked');
+                addossunique.innerHTML = `<span id="heart">&#128163;</span> `
+            }
+            
     }
        
     })
     
 }
 
-//function to se if even ore odd
-// function bombchecker(){
-//     if()
-// }
+
 
 //controll evenen ore odd
 function isEvenOdd(number){
